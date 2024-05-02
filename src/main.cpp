37a5ce1cfe869e0f864e0c9f67e0b69a4bd87a90
Thomaz <thomaz2pinheiro@gmail.com>
2024-05-02 16:33:41 -0300

    3d working

--- src/main.cpp
+++ src/main.cpp
@@ -49,8 +49,6 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    
-    
     GLFWwindow* window = glfwCreateWindow(800, 600, "CG2", NULL, NULL);
     if (window == NULL)
     {
--- src/main2.cpp
+++ src/main2.cpp
@@ -2,6 +2,9 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <math.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include "header/shaderClass.h"
 #include "header/VBO.h"
 #include "header/VAO.h"
@@ -51,19 +54,32 @@ int main() {
 
     GLfloat vertices[] =
     {
-        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-        0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-        0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
-        -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
-        0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
-        0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+        // -0.5f, -0.5f * float(sqrt(3)) / 3,    0.5f,  0.8f, 0.3f, 0.02f,
+        //  0.5f, -0.5f * float(sqrt(3)) / 3,    0.0f,  0.8f, 0.3f, 0.02f,
+        //  0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,  1.0f, 0.6f, 0.32f,
+        // -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,  0.9f, 0.45f, 0.17f,
+        //  0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,  0.9f, 0.45f, 0.17f,
+        //  0.0f, -0.5f * float(sqrt(3)) / 3,    0.5f,  0.8f, 0.3f, 0.02f,
+
+        -0.5f,  0.0f,  0.5f,  0.8f, 0.3f, 0.02f,
+        -0.5f,  0.0f, -0.5f,  0.8f, 0.3f, 0.02f,
+         0.5f,  0.0f, -0.5f,  1.0f, 0.6f, 0.32f,
+         0.5f,  0.0f,  0.5f,  0.9f, 0.45f, 0.17f,
+         0.0f,  0.8f , 0.0f,  0.8f, 0.3f, 0.02f,
     };
 
     GLuint indices[] = 
     {
-        0, 3, 5,
-        3, 2, 4,
-        5, 4, 1
+        // 0, 3, 5,
+        // 3, 2, 4,
+        // 5, 4, 1
+
+        0, 1, 2,
+        0, 2, 3,
+        0, 1, 4,
+        1, 2, 4,
+        2, 3, 4,
+        3, 0, 4
     };
 
     Shader shaderProgram("../resources/shaders/default.vert", "../resources/shaders/default.frag");
@@ -74,71 +90,59 @@ int main() {
     VBO VBO1(vertices, sizeof(vertices));
     EBO EBO1(indices, sizeof(indices));
 
-    VAO1.LinkVBO(VBO1, 0);
+    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     VAO1.Unbind();
     VBO1.Unbind();
     EBO1.Unbind();
 
-    // GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    // glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    // glCompileShader(vertexShader);
+    GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
-    // GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    // glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    // glCompileShader(fragmentShader);
+    float rotation = 0.0f;
+    double prevTime = glfwGetTime();
 
-    // GLuint shaderProgram = glCreateProgram();
-
-    // glAttachShader(shaderProgram, vertexShader);
-    // glAttachShader(shaderProgram, fragmentShader);
-    // glLinkProgram(shaderProgram);
-
-    // glDeleteShader(vertexShader);
-    // glDeleteShader(fragmentShader);
-
-    // GLuint VAO, VBO, EBO;
-
-    // glGenVertexArrays(1, &VAO);
-    // glGenBuffers(1, &VBO);
-    // glGenBuffers(1, &EBO);
-
-    // glBindVertexArray(VAO);
-
-    // glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    // glEnableVertexAttribArray(0);
-
-    // glBindBuffer(GL_ARRAY_BUFFER, 0);
-    // glBindVertexArray(0);
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-    // glClearColor(0.5f, 0.7f, 0.9f, 1.0f);
-    // glClear(GL_COLOR_BUFFER_BIT);
-    // glfwSwapBuffers(window);
+    glEnable(GL_DEPTH_TEST);
 
     while (!glfwWindowShouldClose(window))
     {
         glClearColor(0.5f, 0.7f, 0.9f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         // glUseProgram(shaderProgram);
         shaderProgram.Activate();
+
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 proj = glm::mat4(1.0f);
+
+        double crntTime = glfwGetTime();
+        if (crntTime - prevTime >= 1 / 60)
+        {
+            rotation += 0.05f;
+            prevTime = crntTime;
+        }
+
+        model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+        view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+        proj = glm::perspective(glm::radians(45.0f), (float)(800/800), 0.1f, 100.0f);
+
+        int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+        int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+        int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
+        glUniform1f(uniID, 0.5f);
         // glBindVertexArray(VAO);
         VAO1.Bind();
         // glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
     
-    // glDeleteVertexArrays(1, &VAO);
-    // glDeleteBuffers(1, &VBO);
-    // glDeleteBuffers(1, &EBO);
-    // glDeleteProgram(shaderProgram);
     VAO1.Delete();
     VBO1.Delete();
     EBO1.Delete();
